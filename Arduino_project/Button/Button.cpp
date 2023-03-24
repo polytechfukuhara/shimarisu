d6d0829d2e760b0b1ef746a65efd31e7df5c7188
kumikomi20 <mottyann49ppp@gmail.com>
2023-03-24 12:44:06 +0900

    ボタンのテストのため、pinModeを変更しました。

--- Arduino_project/Button/Button.cpp
+++ Arduino_project/Button/Button.cpp
@@ -3,7 +3,7 @@
 
 Button::Button(int num) {
   button_pin = num;
-  pinMode(button_pin, INPUT_PULLUP);//INPUT_PULLUPを使うと内部のプルアップ抵抗が有効になります。
+  pinMode(button_pin, INPUT_PULLDOWN);//INPUT_PULLUPを使うと内部のプルアップ抵抗が有効になります。
 }
 
 bool Button::buttonRead() {
--- Arduino_project/Button/Button.ino
+++ Arduino_project/Button/Button.ino
@@ -1,5 +1,5 @@
 #include "Button.h"
-#define BUTTON_PIN 2
+#define BUTTON_PIN 15
 
 volatile byte state = HIGH;      // volatileをつけて宣言
 
@@ -8,10 +8,16 @@ Button button(BUTTON_PIN);
 void setup() {
   // ピン番号から割り込み番号への変換には専用の関数を使用
   //attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), blink, CHANGE);
+  Serial.begin(115200);
 }
 
 void loop() {
-
+  if (button.buttonRead()) {
+    Serial.println("true");
+  } else {
+    Serial.println("false");
+  }
+  delay(100);
 }
 
 /*割り込みサービスルーチン(ISR)
